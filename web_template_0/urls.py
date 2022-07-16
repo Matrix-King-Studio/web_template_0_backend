522ed9d3a2cfe969ae55515cd447fd195a90497e
Alex <liu_zhao_feng_alex@163.com>
2022-07-16 16:05:08 +0800

    增加微信扫码登录接口

--- web_template_0/urls.py
+++ web_template_0/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Account.views import wx_login
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("wx_login/")
+	path("admin/", admin.site.urls),
+	path("wx_login/", wx_login),
 ]
