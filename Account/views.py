af41af9480e9839966079875d98be1b03724de7e
liu_zhao_feng_PC <liu_zhao_feng_alex@163.com>
2023-02-25 14:59:44 +0800

    增加了静态文件urlpatterns

--- Account/views.py
+++ Account/views.py
@@ -1,7 +1,7 @@
-from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.weixin.views import WeixinOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 
 
 class WeixinLogin(SocialLoginView):
     adapter_class = WeixinOAuth2Adapter
+    callback_url = "https://www.template.matrix-studio.top/"
