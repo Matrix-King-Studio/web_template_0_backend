74efd3469d06cd58650b8f8420eaf4c711695582
liu_zhao_feng_PC <liu_zhao_feng_alex@163.com>
2023-02-25 11:28:47 +0800

    增加了静态文件urlpatterns

--- Account/models.py
+++ Account/models.py
@@ -1,18 +1,21 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractBaseUser):
-	openid = models.CharField(max_length=64, unique=True)
-	unionId = models.CharField(max_length=64, unique=True)
-	nickname = models.CharField(max_length=64)
-	headImgUrl = models.URLField()
-
-	USERNAME_FIELD = "openid"
-
-	def get_username(self):
-		return self.nickname
-
-	@property
-	def username(self):
-		return self.nickname
+class User(AbstractUser):
+	openid = models.CharField(
+		verbose_name="微信登录用户唯一标识",
+		max_length=128,
+		unique=True,
+		null=True,
+		blank=True,
+		help_text="普通用户的标识，对当前开发者帐号唯一"
+	)
+	unionId = models.CharField(
+		verbose_name="微信登录用户统一标识",
+		max_length=128,
+		unique=True,
+		null=True,
+		blank=True,
+		help_text="针对一个微信开放平台帐号下的应用，同一用户的unionid是唯一的。"
+	)
